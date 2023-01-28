b590315cb1238c2496120dde348fc595fee4d87c
Jan Biernacik <spam@janbiernacik.com>
2023-01-28 13:12:35 +0100

    Added mobile navigation
    + Other enhancements

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ function redirect_home() {
     window.location.href = "de/";
 }
 
+function onload() {
+    console.log("setting variables")
+    menu_expanded = 0;
+    var element = document.getElementById("menu_content");
+    element.style.opacity = "0";
+}
+
 $(window).scroll(function () {
     if ($(window).scrollTop() >= 1) {
         $('.navbar').css('background','#191b24');
@@ -16,3 +23,28 @@ $(window).scroll(function () {
         $('.navbar_links_current').css('color','black');
     }
 });
+
+function menu() {
+    if (menu_expanded == 0) {
+        var element = document.getElementById("menu")
+        element.setAttribute("style", "height: 100vh;")
+        var element = document.getElementById("menu_content")
+        element.setAttribute("style", "height: 100vh;")
+        element.setAttribute("style", "visibility: visible;")
+        setTimeout( () =>{
+            element.style.opacity = "1";
+        }, 250);
+        menu_expanded = 1;
+    }
+    else {
+        var element = document.getElementById("menu_content")
+        element.style.opacity = "0";
+        element.setAttribute("style", "height: 0;")
+        element.setAttribute("style", "visibility: hidden;")
+        var element = document.getElementById("menu")
+        setTimeout( () =>{
+            element.setAttribute("style", "height: 0;")
+        }, 250);
+        menu_expanded = 0;
+    }
+}
